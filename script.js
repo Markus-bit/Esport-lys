9a5ea34d570487de979db0ddc117389a82719633
Markus-bit <majoa006@osloskolen.no>
2022-01-07 13:20:57 +0100

    fix

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ mySlider3.addEventListener("input", update);
 update();
 
 
+
+
+
+
 var colorPicker = new iro.ColorPicker("#picker", {
   // Set the size of the color picker
   width: 320,
@@ -19,30 +23,34 @@ var colorPicker = new iro.ColorPicker("#picker", {
 var colorPicker = new iro.ColorPicker(".colorPicker", {
   // color picker options
   // Option guide: https://iro.js.org/guide.html#color-picker-options
-  width: 200,
+  width: 280,
   color: "rgb(255, 0, 0)",
   borderWidth: 1,
   borderColor: "#000",
 });
 
-
-
+var values = document.getElementById("values");
+var hexInput = document.getElementById("hexInput");
 
 // https://iro.js.org/guide.html#color-picker-events
 // listen to a color picker's color:change event
 // color:change callbacks receive the current color
 colorPicker.on('color:change', function(color) {
-  // log the current color as a HEX string
+  // log the current color as a hsl string
   console.log(color.hslString);
 });
 
 colorPicker.on(["color:init", "color:change"], function(color){
   // Show the current color in different formats
   // Using the selected color: https://iro.js.org/guide.html#selected-color-api
-  colorPicker.color.hsl = { h: 180, s: 100, l:100 };
+  colorPicker.color.hsl = { h: 180, s: 100, l: 50 };
   
+  values.innerHTML = [
+    "hex: " + color.hexString,
+    "rgb: " + color.rgbString,
+    "hsl: " + color.hslString,
+  ].join("<br>");
+  
+  hexInput.value = color.hexString;
 });
 
-hexInput.addEventListener('change', function() {
-  colorPicker.color.hexString = this.value;
-});
